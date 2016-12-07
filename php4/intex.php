4659e7cb81465385f5eccd72552848656d8033a0
Hemul2000 <yh.aleksandr@gmail.com>
2016-12-07 22:05:41 +0300

    Create intex.php

---
+++ php4/intex.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Выборка </title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+      <div class="telo">
+          <p>Гильд Владислав Евгеньевич </p>
+            <p>vlad@mail.ru </p>
+            <p>25.08.1997 </p>
+            <p> 8 (926)8484439</p>
+            <p> </p>
+            <p> </p>
+       </div>
+   </body>
+</html>
