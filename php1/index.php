7df80aa0260252fec628257710cb74ac993a0ab8
Hemul2000 <yh.aleksandr@gmail.com>
2016-12-07 21:57:35 +0300

    Update index.php

--- php1/index.php
+++ php1/index.php
@@ -1,55 +1,75 @@
-<!DOCTYPE.html!>
+<!DOCTYPE html>
 <html>
-<head>
-	<meta = charset = "utf-8">
-	<link rel = "stylesheet" href = "style.css" >
-	<title>Форма Входа</title>
-</head>
-
-<body>
-
-	<?php 
-		$username = "maria";
-		$userpassword = "luchko";
-		$user = 0;
-		if (isset($_POST['name']) && $_POST['name'] == $username){
-				++$user;}
-		if (isset($_POST['password']) && $_POST['password'] == $userpassword){
-				++$user;
-		}
-		if ($user == 2){
-			echo "<meta http-equiv=\"refresh\" content=\"0;URL=index2.html\">";}
-		else {$wrong = '<h2>Неверная пара логин/пароль.</h2>';}
-		?>
-
-	<div class = "forma">
-		<div>
-			<img  src = "1.png" height = "90px">
-			<p>Добро пожалвоать!</p>
-			<p>Введите правильное имя<br>
-			и пароль пользователя для<br>
-			для входа на сайт</p>
+    <head>
+        <meta charset="utf-8">
+        <title>Авторизация</title>
+		<link rel="stylesheet" href="style.css">
+    </head>
+	
+	<body>
+	
+        <div class="fon">
+		
+            <div class="priv">
+                <img src="1.png" height="80px">
+                <p>Добро пожаловать!</p>
+            <p>Введите правильные имя<br> пользователя и пароль<br> для входа на сайт!</p>
+                <br>
+                <a href="#">Регистрация</a>
+            </div>
+				
+				<?php $login = "guru"; $pwd = "12345";
+				if(!isset($_POST['pwd']))
+				{
+					echo '<div class="vhod">			
+                <h2>Вход</h2>				
+				<form method="POST"><div class="auto">			
+					<div class="user">
+                    <b>Имя пользователя</b>
+					<input id="username" maxlength="20" name="username">
+                    </div>
+					
+					<div class="pass">
+                    <b>Пароль</b><br>
+                <input type="password" name="pwd">
+					</div>
+				
+                <input class="but" type="submit" value="Вход">
 			
-			<a href= "#">Регистрация</a>
-		</div>
-		
-		<div class = "forma_vhod">
-		<div class = "forma_fon">
-		<h2>Вход</h2>
-		<div class = "vxod">
-		<p><b>Имя пользователя</b><br><input type = "tex"></p>
-				<p><b>Пароль</b><br><input type = "tex"></p>
-		<br>
-		
-		<input class = "knopka" type="submit" name="auth" value="Вход">
-				        <?php echo $wrong; ?>
-</div>
-		</div>
-		</div>
-
-	</div>
-
-
-</body>
-
+				</div></form>
+				
+            </div>';
+				}
+				else if($_POST['username'] == $login && $_POST['pwd'] == $pwd)
+				{
+					echo 'Privet '.$login;
+					echo '<a href="./index.php">Выход </a>';
+				}
+				else if($_POST['username'] != $login || $_POST['pwd'] != $pwd)
+				{
+					echo '<div class="vhod">			
+                <h2>Вход</h2>				
+				<form method="POST"><div class="auto">			
+					<div class="user">
+					<p>Вы ввели неправельные данные</p>
+                    <b>Имя пользователя</b>
+					<input id="username" maxlength="20" name="username">
+                    </div>
+					
+					<div class="pass">
+                    <b>Пароль</b><br>
+                <input type="password" name="pwd">
+					</div>
+				
+                <input class="but" type="submit" value="Вход">
+			
+				</div></form>
+				
+            </div>';
+				}
+				?>
+				
+           
+        </div>
+    </body>
 </html>
